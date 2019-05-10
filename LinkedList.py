21fde155cc19451a08104d00f11689d2fb2401ee
lundbird <alex.lundberg@gmail.com>
2019-05-10 13:31:36 -0500

    add delete_value

--- LinkedList.py
+++ LinkedList.py
@@ -3,6 +3,7 @@ class Node:
         self.data = data
         self.next = next
 
+
 class LinkedList:
     def __init__(self, data=None):
         self.head = Node(data)
@@ -12,16 +13,17 @@ class LinkedList:
         new_node = Node(data, self.head)
         self.head = new_node
         self.size += 1
-    
+
     def delete_first(self):
         if (self.size < 1):
             raise ArgumentException("LinkedList has no size")
         data = self.head.data
         self.head = self.head.next
+        self.size -= 1
         return data
 
     def add_last(self, data):
-        if (self.size==1):
+        if (self.size == 1):
             self.add_first(data)
         else:
             self.insert(data, self.size-1)
@@ -34,7 +36,7 @@ class LinkedList:
         new_node = Node(data, tmp.next)
         tmp.next = new_node
         self.size += 1
-        
+
     def delete(self, index):
         self.__check_size(index)
         tmp = self.head
@@ -44,6 +46,22 @@ class LinkedList:
         tmp.next = tmp.next.next
         self.size -= 1
         return data
+
+    def delete_value(self, data):
+        #feel like i should be able to refactor this mess
+        tmp = self.head
+        prev = tmp
+        list_size = self.size
+        for i in range(self.size):
+            if tmp.data == data:
+                if (tmp == self.head):
+                    data = self.head.data
+                    self.head = self.head.next
+                prev.next = tmp.next
+                self.size -= 1
+            else:
+                prev = tmp
+            tmp = tmp.next
 
     def get(self, index):
         tmp = self.head
@@ -63,14 +81,14 @@ class LinkedList:
             if tmp.data == data:
                 return True
         return False
-    
+
     def clear(self):
         self.head = None
 
     def print(self):
         tmp = self.head
         for i in range(self.size-1):
-            print(tmp.data, end = ' ')
+            print(tmp.data, end=' ')
             tmp = tmp.next
         print()
 
@@ -78,12 +96,13 @@ class LinkedList:
         if index > self.size:
             raise ValueError("invalid index")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     test = LinkedList()
     for i in range(4):
         test.add_last(i)
     test.print()
-    test.insert(5,1)
+    test.insert(5, 1)
     test.print()
     test.add_last(6)
     test.print()
@@ -91,5 +110,10 @@ if __name__=="__main__":
     print(test.get(1))
     test.delete(2)
     test.print()
-    
-    
+    test.delete_value(5)
+    test.print()
+    test.insert(5, 1)
+    test.insert(5, 1)
+    test.print()
+    test.delete_value(5)
+    test.print()
