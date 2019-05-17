5ce31d897f034a72498ae087d9eaa4e29e70cb98
lundbird <alex.lundberg@gmail.com>
2019-05-17 15:59:04 -0500

    solved but python indices are wierd.

--- palindromic_substrings.py
+++ palindromic_substrings.py
@@ -1,5 +1,15 @@
 def countSubstrings(s):
-    pass
+    count = 0
+    for i in range(1, len(s)+1):
+        for j in range(len(s)-i+1):
+            substr = s[j:j+i]
+            revstr = substr[::-1]
+            # print(substr,revstr)
+            if substr == revstr:
+                count += 1
+    return count
 
-if __name__=="__main__":
-    pass
+
+if __name__ == "__main__":
+    print(countSubstrings("abc"))
+    print(countSubstrings("aaa"))
