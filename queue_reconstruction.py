4d634dc22888e92e5e645a7a659e36a31021c046
lundbird <alex.lundberg@gmail.com>
2019-05-17 14:38:38 -0500

    problem done

--- queue_reconstruction.py
+++ queue_reconstruction.py
@@ -8,6 +8,15 @@ def reconstructQueue(people):
         open_slots.remove(index)
     return result
 
+def reconstructQueue(people):
+    #trick is to realize that if we do the lowest height last they wont change position.
+    #sort descending in terms of height then add to queue with the index
+    people = sorted(people, key = lambda x: (-x[0], x[1]))
+    res = []
+    for p in people:
+        res.insert(p[1], p)
+    return res
+
 if __name__=="__main__":
     test = [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
     print(reconstructQueue(test))
